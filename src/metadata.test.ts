4e214a3455d21b36409ca35f63b422d78c1532a9
Abraham Williams <4braham@gmail.com>
2019-01-19 15:41:41 -0600

    Fix linting rules on metadata test file

--- src/metadata.test.ts
+++ src/metadata.test.ts
@@ -10,22 +10,21 @@ test('returns function', () => {
 });
 
 test('with invalid target', () => {
-  const decorator = Reflect.metadata(metadataKey, metadataValue);
   const target = undefined;
   expect(() => decorator(target, propertyKey)).toThrow(TypeError);
-})
+});
 
 test('with invalid property key', () => {
   const target = {};
-  const propertyKey = {};
-  expect(() => decorator(target, propertyKey)).toThrow(TypeError);
-})
+  const invalidPropertyKey = {};
+  expect(() => decorator(target, invalidPropertyKey)).toThrow(TypeError);
+});
 
 test('with target and without property key', () => {
-  const target = () => {};
-  decorator(target)
+  const target = () => { return; };
+  decorator(target);
   expect(Reflect.hasOwnMetadata(metadataKey, target)).toBeTruthy();
-})
+});
 
 test('with target and propery key', () => {
   const target = {};
